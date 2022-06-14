dbe8c536a552458cd6871933738b401e81b8b5a3
josesoto <josesoto@itop.es>
2022-06-14 18:14:49 +0100

    feat: created user ui

---
+++ src/utils/UserUi.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class UserUi {
+    public static void showMenu(){
+        System.out.println("####################################");
+        System.out.println("      ########################      ");
+        System.out.println("1 - Inicializar Avión");
+        System.out.println("2 - Alternar estado de los flaps");
+        System.out.println("3 - Alternar estado del tren de aterrizaje");
+        System.out.println("4 - Armar sistema de eyección");
+        System.out.println("5.- Eyectar piloto");
+        System.out.println("Q.- Salir");
+        System.out.println("      ########################      ");
+        System.out.println("####################################");
+    }
+}
